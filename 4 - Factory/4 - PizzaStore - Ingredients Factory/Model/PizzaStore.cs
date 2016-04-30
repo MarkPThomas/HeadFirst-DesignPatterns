f2aa4cf1ada7c37a09889004620617313fba28d0
MarkPThomas <markums@gmail.com>
2016-04-29 21:15:30 -0700

    First commit
    Setting up the next step in the project.

---
+++ 4 - Factory/4 - PizzaStore - Ingredients Factory/Model/PizzaStore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaFanchise.Model
+{
+    public abstract class PizzaStore
+    {
+        
+        public Pizza OrderPizza(string type)
+        {
+            Pizza pizza = CreatePizza(type);
+
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            pizza.Box();
+
+            return pizza;
+        }
+
+        protected abstract Pizza CreatePizza(string type);
+    }
+}
---
+++ 4 - Factory/4 - PizzaStore - Ingredients Factory/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PizzaFanchise.Model;
+using PizzaFanchise.Tests;
+
+namespace PizzaFanchise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PizzaTestDrive.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
