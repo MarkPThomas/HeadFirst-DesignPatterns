4888888a843c00112481e88e1fbe14378e118219
MarkPThomas <markums@gmail.com>
2016-04-29 18:49:12 -0700

    First commit. Completed exercise.

---
+++ 3 - Decorator/IODecorator/InputTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IODecorator
+{
+    public class InputTest
+    {
+        public static void Run()
+        {
+            int c;
+
+            try
+            {
+                using (StreamReader stream =
+                        new LowerCaseInputStream(
+                            new BufferedStream(
+                                new FileStream("test.txt", FileMode.Open))))
+                {
+                    while ((c = stream.Read()) >= 0)
+                    {
+                        Console.Write((char)c);
+                    }
+                }        
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3 - Decorator/IODecorator/LowerCaseInputStream.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IODecorator
+{
+    public class LowerCaseInputStream : StreamReader
+    {
+        public LowerCaseInputStream(Stream stream)
+            : base(stream)
+        {
+
+        }
+
+        public override int Read()
+        {
+            try
+            {
+                int c = BaseStream.ReadByte();
+                return (c == -1 ? c : char.ToLower((char)c));
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+
+        }
+
+        public int Read(byte[] b, int offset, int len)
+        {
+            int result = BaseStream.Read(b, offset, len);
+            for (int i = offset; i < offset + result; i++)
+            {
+                b[i] = (byte)char.ToLower((char)b[i]);
+            }
+            return result;
+        }
+    }
+}
